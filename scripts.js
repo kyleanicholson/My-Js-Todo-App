2bd3dbb3cd0939e86bd6c9f6811d7462bd4452d4
Kyle Nicholson <kyleanicholson@gmail.com>
2024-06-17 23:21:15 -0700

    Fix error with blank input

--- scripts.js
+++ scripts.js
@@ -2,44 +2,42 @@ const todos = [
   { text: "Eat breakfast" },
   { text: "Drink coffee" },
   { text: "Contemplate life decisions" },
-]; 
+];
 const todoContainer = document.querySelector("#todo-list");
 const addTodoButton = document.querySelector("#add-todo");
 const todoInput = document.querySelector("#todo-input");
 
 const buildTodoList = (todoList) => {
-  todoContainer.innerHTML = '';
+  todoContainer.innerHTML = "";
   for (let todo of todoList) {
-    let li = createTodoElement(todo)
+    let li = createTodoElement(todo);
     todoContainer.appendChild(li);
   }
 };
 
-const getTodoInputValue = () => {
+const getTodoFromInput = () => {
   let inputVal = todoInput.value;
-  if (inputVal){
+  if (inputVal) {
     console.log(`Input value of add todo ${inputVal}`);
-    return {'text':`${inputVal}`}
-  }
-  else{
-    console.log('Input not found or invalid')
-    return
+    return { text: `${inputVal}` };
+  } else {
+    console.log("Input not found or invalid");
+    return null;
   }
 };
 
 const createTodoElement = (todo) => {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(todo.text));
-    return li
-}
-
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(todo.text));
+  return li;
+};
 
 addTodoButton.addEventListener("click", () => {
-    let todo = getTodoInputValue();
-    todos.push(todo)
+  let todo = getTodoFromInput();
+  if (todo) {
+    todos.push(todo);
     buildTodoList(todos);
-    
-
+  }
 });
 
 buildTodoList(todos);
