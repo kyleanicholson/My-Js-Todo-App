e407f970cc7917543611c8f0d709a2385e2cdd8f
Kyle Nicholson <kyleanicholson@gmail.com>
2024-06-18 17:20:31 -0700

    Add buttons, revise function order, add selection

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
-const todos = [
-  { text: "Eat breakfast" },
-  { text: "Drink coffee" },
-  { text: "Contemplate life decisions" },
-];
+
+// Query selectors
+
+const storedTodos = localStorage.getItem("todos");
 const todoContainer = document.querySelector("#todo-list");
 const addTodoButton = document.querySelector("#add-todo");
 const todoInput = document.querySelector("#todo-input");
 
+
+
+
+// Todo Data
+let todos = []
+if (storedTodos) {
+  todos = JSON.parse(storedTodos);
+} else {
+  todos = [
+    // Example data
+    { text: "Eat breakfast" },
+    { text: "Drink coffee" },
+    { text: "Contemplate life decisions" },
+  ];
+}
+
+// Functions
 const buildTodoList = (todoList) => {
   todoContainer.innerHTML = "";
   for (let todo of todoList) {
     let li = createTodoElement(todo);
+    li.classList.add("todo-item")
     todoContainer.appendChild(li);
   }
+  localStorage.setItem("todos",JSON.stringify(todoList));
+  
+
 };
 
 const getTodoFromInput = () => {
   let inputVal = todoInput.value;
   if (inputVal) {
     console.log(`Input value of add todo ${inputVal}`);
+    todoInput.value = "";
     return { text: `${inputVal}` };
   } else {
     console.log("Input not found or invalid");
@@ -32,6 +53,20 @@ const createTodoElement = (todo) => {
   return li;
 };
 
+buildTodoList(todos);
+
+// Event Listeners
+
+todoInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    let todo = getTodoFromInput();
+    if (todo) {
+      todos.push(todo);
+      buildTodoList(todos);
+    }
+  }
+});
+
 addTodoButton.addEventListener("click", () => {
   let todo = getTodoFromInput();
   if (todo) {
@@ -39,5 +74,21 @@ addTodoButton.addEventListener("click", () => {
     buildTodoList(todos);
   }
 });
+  
 
-buildTodoList(todos);
+
+
+todoItems = document.querySelectorAll(".todo-item")
+todoItems.forEach(todoItem => {
+  todoItem.addEventListener("click", (e) => {
+   
+    let clickedTodo = e.target;
+    if (clickedTodo.classList.contains('selected')){
+      clickedTodo.classList.remove("selected")
+    }
+    else{
+      clickedTodo.classList.add("selected");
+    }
+    
+  });
+});
